23c9ef8c062004a86ba821f811df5b7e004322df
Dagin Gregory <dagin.gregory@gmail.com>
2023-10-17 21:35:18 -0700

    Revert "Added API query logic + tests"
    This reverts commit 0c4b91e0c882ad6bd16a2d030f8ee0ab54f22bc9.

--- src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
+++ src/main/java/edu/ucsb/cs156/spring/backenddemo/services/PublicHolidayQueryService.java
@@ -4,19 +4,12 @@ import org.springframework.web.client.RestTemplate;
 
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Map;
-import java.util.List;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
 import org.springframework.boot.web.client.RestTemplateBuilder;
+
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.HttpClientErrorException;
 
+@Slf4j
 @Service
 public class PublicHolidayQueryService {
 
@@ -27,25 +20,10 @@ public class PublicHolidayQueryService {
         restTemplate = restTemplateBuilder.build();
     }
 
-    public static final String ENDPOINT = "https://date.nager.at/api/v2/publicholidays/{year}/{countryCode}?format=json";
+    public static final String ENDPOINT = "";
 
     public String getJSON(String year, String countryCode) throws HttpClientErrorException {
-        if(Integer.parseInt(year) <= 0)
-            throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Invalid year");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.setAccept(List.of(MediaType.APPLICATION_JSON));
-        headers.setContentType(MediaType.APPLICATION_JSON);
-
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-        Map<String, String> uriVariables = Map.of("year", year, "countryCode", countryCode);
-        ResponseEntity<String> response = restTemplate.exchange(ENDPOINT, HttpMethod.GET, entity, String.class, uriVariables);
-
-        if(response.getStatusCode() == HttpStatusCode.valueOf(200))
-            return response.getBody();
-        
-        //If we didn't get a 2XX response
-        throw new HttpClientErrorException(HttpStatus.BAD_REQUEST, "Ensure the year: " + year + " | and the country code: " + countryCode + " | are valid.");
+        return "";
     }
 
    
